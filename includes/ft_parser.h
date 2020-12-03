d87fce6b1219192a0f1ce53a2e5e31ef47e94006
Dipoo Kenchur <dkenchur@mi-a5.msk.21-school.ru>
2020-12-03 04:24:08 +0300

    ft_parser.h fixed

--- includes/ft_parser.h
+++ includes/ft_parser.h
@@ -6,7 +6,7 @@
 /*   By: dkenchur <dkenchur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 15:35:30 by dkenchur          #+#    #+#             */
-/*   Updated: 2020/12/03 03:17:33 by dkenchur         ###   ########.fr       */
+/*   Updated: 2020/12/03 04:22:51 by dkenchur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,4 @@ int		ft_parser(const char *format, t_specifier *spec);
 void	ft_init_specifier(t_specifier *spec);
 void	follow_line(char **str, t_specifier *spec);
 void	ft_get_star_value(char c, int *i, t_specifier *spec);
-int		isflags(char c);
-int		ismodifer(char c);
 #endif
