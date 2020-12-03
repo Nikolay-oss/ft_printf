70d57f9a3dde4225ac720154edaba81471ecd234
Dipoo Kenchur <dkenchur@mi-a5.msk.21-school.ru>
2020-12-03 03:36:49 +0300

    update

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,13 +6,12 @@
 /*   By: dkenchur <dkenchur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 13:35:59 by dkenchur          #+#    #+#             */
-/*   Updated: 2020/12/03 02:54:20 by dkenchur         ###   ########.fr       */
+/*   Updated: 2020/12/03 03:35:48 by dkenchur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-# include <stdarg.h>
 
 int		ft_printf(const char *format, ...);
 #endif
